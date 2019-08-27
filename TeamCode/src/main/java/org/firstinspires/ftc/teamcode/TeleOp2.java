59ad937afa7ed4dcb9cbaa2ce6184885afacbbfd
Anthony Zhao <anthonyrzhao@gmail.com>
2019-08-27 12:55:19 -0400

    Added TeleOp2 File
    TeleOp2 Created

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class TeleOp2 extends LinearOpMode {
+    //This version has better controls for the new drivetrain
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
