04e18f565116ccfe60475c8ffcfc790657193389
Anthony Zhao <anthonyrzhao@gmail.com>
2019-08-27 12:58:08 -0400

    Deleted a comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class TeleOp2 extends LinearOpMode {
-    //This version has better controls for the new drivetrain
     @Override
     public void runOpMode() throws InterruptedException {
 
