b10e59b13f0fbc9433d4a1b48057ee466daa0606
Anthony Zhao <anthonyrzhao@gmail.com>
2019-08-27 12:30:15 -0400

    Deleted Alpha, Added LinearOpMode DepotSide

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DepotSide.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class DepotSide extends LinearOpMode {
+    //DEPOTSIDE IS THE BEST SIDE
+    @Override
+    public void runOpMode() {
+
+    }
+}
